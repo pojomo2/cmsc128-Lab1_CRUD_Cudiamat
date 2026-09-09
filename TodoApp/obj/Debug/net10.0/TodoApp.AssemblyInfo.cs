ea20b8ffd76bad867b45a99e10738c584a639589
Drew T. Cudiamat <dtcudiamat@up.edu.ph>
2026-09-09 14:27:53 +0800

    Due dates can now be inputted per task; also fixed vertical alignment of task title and priority.

--- TodoApp/obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
+++ TodoApp/obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb10a0f62c5b55b544b135cbcd61b719f309f61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7c1cc25b929ddc72426d256d89bb3b6046ec16")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
